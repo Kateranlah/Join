8393093d5ee080fe4e9ce532783981844a54d447
Kevin Sven Lentz <kevin.s.lentz@web.de>
2023-01-18 14:19:34 +0100

    dropdowncloses whereever you click

--- script/templates.js
+++ script/templates.js
@@ -1,55 +1,65 @@
+let dropdownOpen = false;
+
 async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "includes/header.html"
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
+  let includeElements = document.querySelectorAll("[w3-include-html]");
+  for (let i = 0; i < includeElements.length; i++) {
+    const element = includeElements[i];
+    file = element.getAttribute("w3-include-html"); // "includes/header.html"
+    let resp = await fetch(file);
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = "Page not found";
     }
-    showCurrentPage();
+  }
+  showCurrentPage();
 }
 
 function showCurrentPage() {
-    document.getElementById(getID(currentSite())).classList.add('activeSite');
-    document.getElementById(getID(currentSite())).style.pointerEvents = 'none';
-    document.getElementById(getID(currentSite())).style.userSelect = 'none';
-
+  document.getElementById(getID(currentSite())).classList.add("activeSite");
+  document.getElementById(getID(currentSite())).style.pointerEvents = "none";
+  document.getElementById(getID(currentSite())).style.userSelect = "none";
 }
 
 /**
  * @returns current html site
  */
 function currentSite() {
-    let currentSite = window.location.href;
-    currentSite = currentSite.substring(currentSite.lastIndexOf('/'));
-    return currentSite;
+  let currentSite = window.location.href;
+  currentSite = currentSite.substring(currentSite.lastIndexOf("/"));
+  return currentSite;
 }
 
 function getID(path) {
-    const pathWithoutHTML = path.split('.html')[0]
-    const id = pathWithoutHTML.slice(1);
-    return id;
+  const pathWithoutHTML = path.split(".html")[0];
+  const id = pathWithoutHTML.slice(1);
+  return id;
 }
 
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".log-out") && dropdownOpen) {
+    document.getElementById("log-out").classList.add("d-none");
+    dropdownOpen = false;
+  }
+};
 
 /**
  * function displays the log out button if profile pic is clicked
  */
 function showLogOut() {
-    let logOut = document.getElementById('log-out');
-    if (logOut.classList.contains('d-none')) {
-        document.getElementById('log-out').classList.remove('d-none');
-    } else {
-        document.getElementById('log-out').classList.add('d-none');
-    }
+  let logOut = document.getElementById("log-out");
+  if (logOut.classList.contains("d-none")) {
+    document.getElementById("log-out").classList.remove("d-none");
+    setTimeout(() => {
+      dropdownOpen = true;
+    }, 200);
+  } else {
+    document.getElementById("log-out").classList.add("d-none");
+    dropdownOpen = false;
+  }
 }
-
 
 function logOut() {
-    window.location.href = 'index.html';
+  window.location.href = "index.html";
 }
-
